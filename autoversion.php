5f19a7fb060ddb0f8941e31940b0a6bcf7888b27
Ted <tedshd@gmail.com>
2015-08-16 11:26:37 +0800

    Update autoversion.php

--- autoversion.php
+++ autoversion.php
@@ -1,9 +1,9 @@
 <?php
 function autoversion($url) {
     $ver = stat($_SERVER['DOCUMENT_ROOT'] . $url)[mtime];
-    return  $url . "?v=" .$ver;
+    return $url . "?v=" . $ver;
 }
 # example
 ?>
 
-<link href="<?php autoversion('/path/to/theme.css'); ?>" rel="stylesheet">
+<link href="<?php echo autoversion('/path/to/theme.css'); ?>" rel="stylesheet">
